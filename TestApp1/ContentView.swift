e179e26dfd229746e4f954236022ac44352c08d5
Artem Morozov <Artem@MacBook-Pro.local>
2023-04-07 08:35:55 +0300

    Test commit

---
+++ TestApp1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TestApp1
+//
+//  Created by Artem Morozov on 13.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+                
+                
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestApp1/TestApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp1App.swift
+//  TestApp1
+//
+//  Created by Artem Morozov on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
